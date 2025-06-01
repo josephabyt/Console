b9108a036febeca3891f707e6a071acd2e15001e
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-05-31 23:56:17 -0400

    Coroutinemanager on player spawned

--- Plugin.cs
+++ Plugin.cs
@@ -18,6 +18,7 @@ namespace Console
             if (ConsoleObject == null)
             {
                 ConsoleObject = new GameObject(ConsoleGUID);
+                ConsoleObject.AddComponent<CoroutineManager>();
                 ConsoleObject.AddComponent<Console>();
             }
 
